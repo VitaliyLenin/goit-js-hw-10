1a92c2997fd5dd0046e61f7a0f791977ddcaf3e0
VitaliyLenin <vitaliylenin77@gmail.com>
2022-12-10 23:31:53 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import './css/styles.css';
+import { fetchCountries } from './fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
+
+const refs = {
+  input: document.querySelector('#search-box'),
+  countryList: document.querySelector('.country-list'),
+  countryInfo: document.querySelector('.country-info'),
+};
