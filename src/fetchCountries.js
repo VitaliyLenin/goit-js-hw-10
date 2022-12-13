6971b0abe5dc2bcd099f8254f50f05455bf81c22
VitaliyLenin <vitaliylenin77@gmail.com>
2022-12-13 03:04:04 +0200

    Update fetchCountries.js

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,16 +1,14 @@
 const BASE_URL = 'https://restcountries.com/v3.1/';
 
 export function fetchCountries(name) {
-  fetch(
-    '${BASE_URL}/name/${name}?fields=name,capital,pupulation,flags.languages'
-  )
-    .then(response => {
-      return response.json();
-    })
-    .then(countries => {
-      console.log(countries);
-    })
-    .catch(error => {
-      console.log(error);
-    });
+  return fetch(`${BASE_URL}name/${name}?fields=${options}`).then(response => {
+    if (!response.ok) {
+      throw new Error('Oops, there is no country with that name');
+    }
+    return response.json();
+  });
 }
+
+const options = ['name', 'capital', 'population', 'flags', 'languages'].join(
+  ','
+);
